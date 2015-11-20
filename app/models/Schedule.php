d0854038f31a5c4899f01fa8c40ad2d37504b44a
Mengdu Li <mengduli1990@gmail.com>
2015-11-19 23:24:12 -0400

    make id fillable

--- app/models/Schedule.php
+++ app/models/Schedule.php
@@ -2,7 +2,7 @@
 
 class Schedule extends Eloquent {
 
-	protected $fillable = array('day', 'start_at', 'end_at');
+	protected $fillable = array('day', 'start_at', 'end_at', 'ta1_id', 'ta2_id');
 
 	public function user()
 	{
